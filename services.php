9885d7e4988080fe51882ac4f0717a3c321ece40
alsi <syahrulhusna@gmail.com>
2022-05-31 09:19:18 +0700

    serpis atas

--- services.php
+++ services.php
@@ -2,19 +2,19 @@
         <div class="container">
 
             <div class="section-title" data-aos="fade-up">
-                <h2>Sekitar Beruang</h2>
+                <h2>Facts about Polar Bear</h2>
                 <p>Mari kita pelajari tentang Beruang</p>
             </div>
 
             <div class="row">
                 <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in">
                     <div class="icon-box icon-box-blue">
-                    <a href="classification.php">   
-                        <div class="">
-                            <img src="https://images.unsplash.com/photo-1590787996529-a542c86ca267?b=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="card-img-top card-cover" height="150em" alt="...">
-                        </div>
-                        <h4 class="title">Classification</h4>
-                        <p class="description" style="text-align: justify;">Beruang Kutub (Ursus maritimus) adalah spesies beruang besar hidup di kutub utara. Beruang ini mampu berenang sejauh 60 mil tanpa henti'</p>
+                        <a href="classification.php">
+                            <div class="">
+                                <img src="https://images.unsplash.com/photo-1590787996529-a542c86ca267?b=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="card-img-top card-cover" height="150em" alt="...">
+                            </div>
+                            <h4 class="title">Classification</h4>
+                            <p class="description" style="text-align: justify;">Beruang Kutub (Ursus maritimus) adalah spesies beruang besar hidup di kutub utara. Beruang ini mampu berenang sejauh 60 mil tanpa henti'</p>
                         </a>
                     </div>
                 </div>
@@ -31,25 +31,25 @@
 
                 <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
                     <div class="icon-box icon-box-blue">
-                    <a href="behaviour.php">   
-                        <div class="">
-                            <img src="assets/img/bear/b.jpeg" class="card-img-top card-cover" height="150em" alt="...">
-                        </div>
-                        <h4 class="title"><a href="">Behaviour</a></h4>
-                        <p class="description" style="text-align: justify;">Kebanyakan beruang coklat dewasa bersifat krepuskular, dengan aktivitas puncak pada pagi dan sore hari. Beruang muda mungkin aktif di siang hari, sementara beruang yang hidup di dekat manusia cenderung aktif di malam hari. </p>
-                    </a>
+                        <a href="behaviour.php">
+                            <div class="">
+                                <img src="assets/img/bear/b.jpeg" class="card-img-top card-cover" height="150em" alt="...">
+                            </div>
+                            <h4 class="title"><a href="">Behaviour</a></h4>
+                            <p class="description" style="text-align: justify;">Kebanyakan beruang coklat dewasa bersifat krepuskular, dengan aktivitas puncak pada pagi dan sore hari. Beruang muda mungkin aktif di siang hari, sementara beruang yang hidup di dekat manusia cenderung aktif di malam hari. </p>
+                        </a>
                     </div>
                 </div>
 
                 <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="300">
                     <div class="icon-box icon-box-blue">
-                    <a href="habitat.php">   
-                        <div class="">
-                            <img src="assets/img/bear/c.jpeg" class="card-img-top card-cover" height="150em" alt="...">
-                        </div>
-                        <h4 class="title"><a href="">Habitat</a></h4>
-                        <p class="description" style="text-align: justify;">Secara umum, Beruang kutub hidup secara individual. Beruang betina biasanya akan hidup di sepanjang pantai Kutub Utara, sementara beruang jantan tinggal di atas bongkahan - bongkahan es dan terapung - apung di laut sekitar 180 mil jauhnya dari pantai.</p>
-                    </a>
+                        <a href="habitat.php">
+                            <div class="">
+                                <img src="assets/img/bear/c.jpeg" class="card-img-top card-cover" height="150em" alt="...">
+                            </div>
+                            <h4 class="title"><a href="">Habitat</a></h4>
+                            <p class="description" style="text-align: justify;">Secara umum, Beruang kutub hidup secara individual. Beruang betina biasanya akan hidup di sepanjang pantai Kutub Utara, sementara beruang jantan tinggal di atas bongkahan - bongkahan es dan terapung - apung di laut sekitar 180 mil jauhnya dari pantai.</p>
+                        </a>
                     </div>
                 </div>
 
