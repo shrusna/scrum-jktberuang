78f976d8c6280a99b458db687e0f3ed5060480c4
Rinaldito <rinalditoryanari@gmail.com>
2022-05-31 09:11:12 +0700

    habitat

---
+++ habitat-about.php
@@ -0,0 +1,34 @@
+ <section id="about" class="about">
+     <div class="container">
+         <div class="row">
+             <div class="col-lg-6" data-aos="zoom-in">
+                 <img src="https://www.researchgate.net/profile/Dorothea-Born/publication/323466669/figure/fig1/AS:599219891077120@1519876563394/Norbert-Rosing-National-Geographic-Creative-1998.png" class="img-fluid" alt="" />
+             </div>
+             <div class="col-lg-6 d-flex flex-column justify-contents-center" data-aos="fade-left">
+                 <div class="content pt-4 pt-lg-0" style="padding-bottom: 0rem;">
+                     <h3>Polar Bear ( Ursus maritimus )</h3>
+                     <p class="fst-italic">
+                     Polar bears are found in the Arctic Circle and adjacent land masses as far south as Newfoundland. While they are rare north of 88°, there is evidence that they range all the way across the Arctic, and as far south as James Bay in Canada.                      
+                     </p>
+                     <ul>
+                         <li>
+                             <i class="bi bi-check-circle"></i>CONTINENTS: North America, Europe, Asia
+
+                         </li>
+                         <li>
+                             <i class="bi bi-check-circle"></i>COUNTRIES: Canada, Norway, Russia, United States
+                         </li>
+                         <li>
+                             <i class="bi bi-check-circle"></i>REGIONS: Labrador, Manitoba, Newfoundland, Northwest Territories, Nunavut, Ontario, Québec, Yukon, Greenland, Yakutiya,</li>
+                         <li>
+                             <i class="bi bi-check-circle"></i>OCEANS: Arctic Ocean
+                         </li>
+                         <li>
+                             <i class="bi bi-check-circle"></i>BIOGEOGRAPHICAL REALMS: Nearctic, Palearctic
+                        </li>
+                     </ul>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
--- services.php
+++ services.php
@@ -41,11 +41,13 @@
 
                 <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="300">
                     <div class="icon-box icon-box-blue">
+                    <a href="habitat.php">   
                         <div class="">
                             <img src="assets/img/bear/c.jpeg" class="card-img-top card-cover" height="150em" alt="...">
                         </div>
                         <h4 class="title"><a href="">Habitat</a></h4>
                         <p class="description" style="text-align: justify;">Secara umum, Beruang kutub hidup secara individual. Beruang betina biasanya akan hidup di sepanjang pantai Kutub Utara, sementara beruang jantan tinggal di atas bongkahan - bongkahan es dan terapung - apung di laut sekitar 180 mil jauhnya dari pantai.</p>
+                    </a>
                     </div>
                 </div>
 
